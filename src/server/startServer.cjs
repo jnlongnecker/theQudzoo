92a70e24707c1778ca4c970ff92de6fb832afb96
jnlongnecker <jlnecker1@gmail.com>
2023-01-11 15:38:54 -0500

    Server mode fix

--- src/server/startServer.cjs
+++ src/server/startServer.cjs
@@ -8,6 +8,13 @@ const app = lwrServer.getInternalServer("express");
 app.get("/api/:info", (req, res) => {
     api.processRequest(res, req.params.info).then(result => res.json(result));
 });
+let mode = "dev";
+
+if (process.argv.length >= 3) {
+    mode = process.argv[2];
+}
+
+lwrServer.config.serverMode = mode;
 
 lwrServer.listen(({ port, serverMode }) => {
     console.log(`App listening on port ${port} in ${serverMode} mode\n`);
