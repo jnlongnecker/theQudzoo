c086987aeafbb97f2f7aa71d5338f6ba31cd0c1e
jnlongnecker <jlnecker1@gmail.com>
2024-08-16 13:15:29 -0400

    Settings bar on mobile now does not overflow
    On mobile, the build help button now moves below the settings bar to allow for more space

--- __lwr_cache__/lwc_compiled_modules/cache/build_library_library_html_0_0_1.js
+++ __lwr_cache__/lwc_compiled_modules/cache/build_library_library_html_0_0_1.js
@@ -12,7 +12,7 @@ import _buildGhostBox from "build/ghostBox";
 import {registerTemplate} from "lwc";
 function tmpl($api, $cmp, $slotset, $ctx) {
   const {d: api_dynamic_text, t: api_text, h: api_element, b: api_bind, c: api_custom_element, k: api_key, i: api_iterator, f: api_flatten} = $api;
-  const {_m0, _m1, _m2, _m3, _m4, _m5, _m6, _m7} = $ctx;
+  const {_m0, _m1, _m2, _m3, _m4, _m5, _m6} = $ctx;
   return [api_element("div", {
     classMap: {
       "shadow": true
@@ -50,6 +50,9 @@ function tmpl($api, $cmp, $slotset, $ctx) {
     },
     key: 6
   }, [api_element("a", {
+    classMap: {
+      "non-mobile-help": true
+    },
     attrs: {
       "href": "/build-code"
     },
@@ -61,71 +64,57 @@ function tmpl($api, $cmp, $slotset, $ctx) {
     attrs: {
       "tabindex": "-1"
     },
-    key: 8,
-    on: {
-      "click": _m1 || ($ctx._m1 = api_bind($cmp.showPopup))
-    }
+    key: 8
   }, [api_element("p", {
     classMap: {
       "help-text": true
     },
     key: 9
-  }, [api_text("How Do I Add A Build Code?")]), api_element("span", {
-    classMap: {
-      "help-icon": true
-    },
-    key: 10
-  }, [api_custom_element("input-icon", _inputIcon, {
-    props: {
-      "variant": "question",
-      "size": "normal"
-    },
-    key: 11
-  }, [])])])]), api_element("button", {
+  }, [api_text("How Do I Add A Build Code?")])])]), api_element("button", {
     className: $cmp.filterClass,
-    key: 12,
+    key: 10,
     on: {
-      "click": _m2 || ($ctx._m2 = api_bind($cmp.toggleFilters))
+      "click": _m1 || ($ctx._m1 = api_bind($cmp.toggleFilters))
     }
   }, [api_custom_element("input-icon", _inputIcon, {
     props: {
       "variant": "filter",
       "size": "normal"
     },
-    key: 13
+    key: 11
   }, [])])])]), !$cmp.hideFilters ? api_element("div", {
     classMap: {
       "sort": true
     },
-    key: 14
+    key: 12
   }, [api_element("div", {
     classMap: {
       "horizontal-spread": true
     },
-    key: 15
+    key: 13
   }, [api_element("div", {
     classMap: {
       "option": true
     },
-    key: 16
+    key: 14
   }, [api_element("label", {
-    key: 17
+    key: 15
   }, [api_text("Genotype")]), api_custom_element("input-picklist", _inputPicklist, {
     props: {
       "options": "Any,Mutated Human,True Kin",
       "chosen": $cmp.genotypeFilter
     },
-    key: 18,
+    key: 16,
     on: {
-      "change": _m3 || ($ctx._m3 = api_bind($cmp.updateGenotype))
+      "change": _m2 || ($ctx._m2 = api_bind($cmp.updateGenotype))
     }
   }, [])]), api_element("div", {
     classMap: {
       "option": true
     },
-    key: 19
+    key: 17
   }, [api_element("label", {
-    key: 20
+    key: 18
   }, [api_text("Sort Order")]), api_custom_element("input-switch", _inputSwitch, {
     props: {
       "left": "Ascending",
@@ -134,63 +123,86 @@ function tmpl($api, $cmp, $slotset, $ctx) {
       "checkcolor": "mutant",
       "checked": $cmp.switchChecked
     },
-    key: 21,
+    key: 19,
     on: {
-      "switch": _m4 || ($ctx._m4 = api_bind($cmp.updateOrder))
+      "switch": _m3 || ($ctx._m3 = api_bind($cmp.updateOrder))
     }
   }, [])]), api_element("div", {
     classMap: {
       "option": true
     },
-    key: 22
+    key: 20
   }, [api_element("label", {
-    key: 23
+    key: 21
   }, [api_text("Sort By")]), api_custom_element("input-picklist", _inputPicklist, {
     props: {
       "options": "Likes,Created Date,Last Updated",
       "chosen": $cmp.sortBy
     },
-    key: 24,
+    key: 22,
     on: {
-      "change": _m5 || ($ctx._m5 = api_bind($cmp.updateSort))
+      "change": _m4 || ($ctx._m4 = api_bind($cmp.updateSort))
     }
   }, [])])]), api_element("div", {
     classMap: {
       "tags": true
     },
-    key: 25
+    key: 23
   }, api_flatten([api_element("label", {
-    key: 26
+    key: 24
   }, [api_text("Tags")]), api_iterator($cmp.tags, function (tag) {
     return api_custom_element("build-tag", _buildTag, {
       props: {
         "label": tag
       },
-      key: api_key(27, tag),
+      key: api_key(25, tag),
       on: {
-        "click": _m6 || ($ctx._m6 = api_bind($cmp.changeTag))
+        "click": _m5 || ($ctx._m5 = api_bind($cmp.changeTag))
       }
     }, []);
   })]))]) : null])]), api_element("section", {
     classMap: {
       "container": true
     },
+    key: 26
+  }, api_flatten([api_element("div", {
+    classMap: {
+      "mobile-help-button": true
+    },
+    key: 27
+  }, [api_element("a", {
+    attrs: {
+      "href": "/build-code"
+    },
     key: 28
-  }, api_flatten([api_iterator($cmp.builds, function (build) {
+  }, [api_element("button", {
+    classMap: {
+      "setting-button": true
+    },
+    attrs: {
+      "tabindex": "-1"
+    },
+    key: 29
+  }, [api_element("p", {
+    classMap: {
+      "help-text": true
+    },
+    key: 30
+  }, [api_text("How Do I Add A Build Code?")])])])]), api_iterator($cmp.builds, function (build) {
     return api_custom_element("build-card", _buildCard, {
       props: {
         "mode": $cmp.mode,
         "build": build,
         "contextid": $cmp.contextUserId
       },
-      key: api_key(29, build._id),
+      key: api_key(31, build._id),
       on: {
-        "deletedbuild": _m7 || ($ctx._m7 = api_bind($cmp.fetchBuilds))
+        "deletedbuild": _m6 || ($ctx._m6 = api_bind($cmp.fetchBuilds))
       }
     }, []);
   }), api_iterator($cmp.ghostBuilds, function (ghost) {
     return $cmp.working ? api_custom_element("build-ghost-box", _buildGhostBox, {
-      key: api_key(30, ghost)
+      key: api_key(32, ghost)
     }, []) : null;
   })]))];
 }
